c8ec5f6825b8d05f0aba4776810238171902b65a
mdaffayusuff <95558620+mdaffayusuff@users.noreply.github.com>
2023-03-22 09:28:15 +0700

    Source project code

---
+++ Source Code/src/daftarmahasiswa/Mahasiswa.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package daftarmahasiswa;
+
+/**
+ *
+ * @author sekar
+ */
+public class Mahasiswa {
+    private String nim;
+    private String nama;
+    private String nilai;
+    private String prodi;
+    
+    public Mahasiswa(String nim, String nama, String nilai, String prodi){
+        this.nim = nim;
+        this.nama = nama;
+        this.nilai = nilai;
+        this.prodi = prodi;
+    }
+    
+    public void setNim(String nim) {
+        this.nim = nim;
+    }
+    
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+    
+    public void setNilai(String nilai) {
+        this.nilai = nilai;
+    }
+    
+    public void setProdi(String prodi) {
+        this.prodi = prodi;
+    }
+    
+    public String getNim() {
+        return this.nim;
+    }
+    
+    public String getNama() {
+        return this.nama;
+    }
+    
+    public String getNilai() {
+        return this.nilai;
+    }
+    
+    public String getProdi() {
+        return this.prodi;
+    }
+}
